ea9fcaf9ba33b8bdc357f6687a0ac1dedc9e081f
baldwin <alexbaldwinpub@gmail.com>
2025-04-07 18:49:18 +0000

    Correction des erreurs pour Render

--- app.py
+++ app.py
@@ -54,3 +54,8 @@ def get_file(filename):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+from flask import render_template
+
+@app.route('/interface')
+def interface():
+    return render_template('index.html')
